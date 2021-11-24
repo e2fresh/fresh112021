fe12c5097e9759f5e8a7c4ca83acae77ae9cafc6
e2fresh <iot@e2energiaeficiente.com>
2021-11-24 16:38:37 -0500

    ya se

--- modbus.py
+++ modbus.py
@@ -6,7 +6,7 @@ from pymodbus.transaction import ModbusRtuFramer
 from calculo_tot2 import energiaTot as energia
 import math
 import struct
-#entiendo
+
 class modbusRTU:
     def __init__(self, device):         
         self.device=device     
